6ac1260a2269ae75927bd30f97fd8ac486702b2a
LeticiaPassold <110414761+LeticiaPassold@users.noreply.github.com>
2023-06-29 16:01:58 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,22 @@
 import { NgModule } from '@angular/core';
-
 import { BrowserModule } from '@angular/platform-browser';
-
-
 
 import { AppComponent } from './app.component';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
 
-
-
-import { environment } from '../environment';
-
-
-
-import { AngularFireModule } from '@angular/fire/compat';
-
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-
-
-
+import { enviroment }  from '../enviroment';
+import { ListContatoComponent } from './contato/list-contato/list-contato.component';
 import { AddContatoComponent } from './contato/add-contato/add-contato.component';
-
 import { EditContatoComponent } from './contato/edit-contato/edit-contato.component';
 
-import { ListContatoComponent } from './contato/list-contato/list-contato.component';
-
-import {  BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { NgxPaginationModule } from 'ngx-pagination';
-
-
 
 @NgModule({
 
@@ -52,7 +36,7 @@ import { NgxPaginationModule } from 'ngx-pagination';
 
     BrowserModule,
 
-    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireModule.initializeApp(enviroment.firebaseConfig),
 
     AngularFireDatabaseModule,
 
